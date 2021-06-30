9eca3e2853e91b560e06e4597caf8fab19a9c98c
Rakesh Vardan <rakeshlearnit@gmail.com>
2021-06-30 17:38:28 +0530

    initial commit2

---
+++ src/test/java/com/lambdatest/SampleTestFF.java
@@ -0,0 +1,32 @@
+package com.lambdatest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+
+public class SampleTestFF {
+
+    public RemoteWebDriver driver = null;
+
+    @Test
+    public void testScript() {
+        try {
+
+            FirefoxOptions options = new FirefoxOptions();
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+            driver.get("https://lambdatest.github.io/sample-todo-app/");
+            driver.findElement(By.name("li1")).click();
+            driver.findElement(By.name("li2")).click();
+            driver.findElement(By.id("sampletodotext")).clear();
+            driver.findElement(By.id("sampletodotext")).sendKeys("Yey, Let's add it to list");
+            driver.findElement(By.id("addbutton")).click();
+            driver.quit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
