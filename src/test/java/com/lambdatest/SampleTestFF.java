ca63ac9bace295e27e396239fe2361a34934ad1b
Rakesh Vardan <rakeshlearnit@gmail.com>
2021-07-11 18:12:39 +0530

    tets message

--- src/test/java/com/lambdatest/SampleTestFF.java
+++ src/test/java/com/lambdatest/SampleTestFF.java
@@ -3,6 +3,7 @@ package com.lambdatest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
@@ -12,12 +13,27 @@ public class SampleTestFF {
 
     public RemoteWebDriver driver = null;
 
+    String username = System.getenv("LT_USERNAME") != null ? System.getenv("LT_USERNAME") : "test";
+    String accessKey = System.getenv("LT_ACCESS_KEY") != null ? System.getenv("LT_ACCESS_KEY") : "test";
+
     @Test
     public void testScript() {
         try {
 
-            FirefoxOptions options = new FirefoxOptions();
-            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("platform", "Windows 10");
+            capabilities.setCapability("browserName", "Firefox");
+            capabilities.setCapability("version", "89.0"); // If this cap isn't specified, it will just get the any available one
+            capabilities.setCapability("resolution","1024x768");
+            capabilities.setCapability("build", "New Test case");
+            capabilities.setCapability("name", "Brand new test case");
+            capabilities.setCapability("network", true); // To enable network logs
+            capabilities.setCapability("visual", true); // To enable step by step screenshot
+            capabilities.setCapability("video", true); // To enable video recording
+            capabilities.setCapability("console", true); // To capture console logs
+
+//            FirefoxOptions options = new FirefoxOptions();
+            driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.lambdatest.com/wd/hub"), capabilities);
             driver.get("https://lambdatest.github.io/sample-todo-app/");
             driver.findElement(By.name("li1")).click();
             driver.findElement(By.name("li2")).click();
